acda19339e7bfb6e48e89daca4b25ae23ecc979a
desire627 <desireasiimwe24@gmail.com>
2024-10-21 15:03:25 +0300

    edited again

--- test/test.js
+++ test/test.js
@@ -11,3 +11,13 @@ it('should return 4 when multiplying 2 and 2', () => {
     assert.equal(multiply(2, 2), 4);
   });
   
+
+  it('should return 9 when multiplying 3 and 3', () => {
+    assert.equal(multiply(3, 3), 9);
+  });
+  
+
+  it('should return 16 when multiplying 4 and 4', () => {
+    assert.equal(multiply(4, 4), 16);
+  });
+  
